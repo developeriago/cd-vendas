e21ef1c1230b2e63a1cc2cc1492e64100eda687f
Iago Almeida <iagoalmeidadev@gmail.com>
2022-06-20 17:52:17 -0300

    Criado tela de atualização e visualização

--- server/controllers/clienteController.js
+++ server/controllers/clienteController.js
@@ -17,7 +17,7 @@ exports.listar = (req, res)=>{
         console.log('Connected as ID' + connection.threadid);
 
         //cliente the connection
-        connection.query('select * from clientes', (err, rows)=>{
+        connection.query('select * from clientes where status = "ativo"', (err, rows)=>{
             connection.release();
 
             if(!err){
@@ -58,4 +58,92 @@ exports.buscar = (req, res)=>{
 
 exports.form = (req, res)=>{
     res.render('add-cliente');
+}
+
+exports.create = (req, res)=>{
+    const {nome, telefone, data_compra, total_compra, tipo_pagamento, observacao} = req.body;
+
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log('Connected as ID' + connection.threadid);
+
+        let seachTerm = req.body.buscar;
+
+        //cliente the connection
+        connection.query('INSERT INTO clientes SET nome = ?, telefone = ?, data_compra = ?, total_compra = ?, tipo_pagamento = ?, observacao = ? ', 
+            [nome, telefone, data_compra, total_compra, tipo_pagamento, observacao], (err, rows)=>{
+            connection.release();
+
+            if(!err){
+                res.render('add-cliente', { alert: 'Cliente adicionado!' });
+            }else{
+                console.log(err);
+            }
+
+            console.log('The data from cliente table: \n', rows);
+        });
+    });
+}
+
+//Visualizar Cliente
+exports.edit = (req, res)=>{
+
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log('Connected as ID' + connection.threadid);
+
+        //cliente the connection
+        connection.query('select * from clientes where id = ?', [req.params.id] , (err, rows)=>{
+            connection.release();
+
+            if(!err){
+                res.render('edit-cliente', { rows });
+            }else{
+                console.log(err);
+            }
+
+            console.log('The data from cliente table: \n', rows);
+        });
+    });
+}
+
+//Atualizar Cliente
+exports.atualizar = (req, res)=>{
+    const {nome, telefone, data_compra, total_compra, tipo_pagamento, observacao} = req.body;
+
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log('Connected as ID' + connection.threadid);
+
+        //cliente the connection
+        connection.query('UPDATE clientes SET nome = ?, telefone = ? where id = ?', [nome, telefone, req.params.id] , (err, rows)=>{
+            connection.release();
+
+            if(!err){
+                pool.getConnection((err, connection)=>{
+                    if(err) throw err;
+                    console.log('Connected as ID' + connection.threadid);
+            
+                    //cliente the connection
+                    connection.query('select * from clientes where id = ?', [req.params.id] , (err, rows)=>{
+                        connection.release();
+            
+                        if(!err){
+                            res.render('edit-cliente', { rows });
+                        }else{
+                            console.log(err);
+                        }
+            
+                        console.log('The data from cliente table: \n', rows);
+                    });
+                });
+
+
+            }else{
+                console.log(err);
+            }
+
+            console.log('The data from cliente table: \n', rows);
+        });
+    });
 }
--- server/routes/cliente.js
+++ server/routes/cliente.js
@@ -5,5 +5,8 @@ const clienteController = require('../controllers/clienteController');
 router.get('/', clienteController.listar);
 router.post('/', clienteController.buscar);
 router.get('/addcliente', clienteController.form);
+router.post('/addcliente', clienteController.create);
+router.get('/editcliente/:id', clienteController.edit);
+router.post('/editcliente/:id', clienteController.atualizar);
 
 module.exports = router;
